26c3cc8b8e3dd37cb162aef723d5a05877d676ed
Bianka Passos <biankatpas@gmail.com>
2018-10-08 12:10:32 -0300

    criando repo

---
+++ src/res/server/Contador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package res.server;
+
+/**
+ *
+ * @author biankatpas
+ */
+import res.ContadorDistribuido;
+import java.rmi.RemoteException;
+
+public class Contador implements ContadorDistribuido{
+
+    private int valor = 0;
+
+    @Override
+    public void incrementa() throws RemoteException {
+        this.valor++;
+    }
+
+    @Override
+    public int obtemValorAtual() throws RemoteException {
+        return this.valor;
+    }
+}
